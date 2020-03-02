47aef9c358db0c1ebdeb952dfef23bd47f772573
Klaudia <klaudia.krzywda@gmail.com>
2020-03-02 22:07:49 +0000

    Updates

---
+++ precache-manifest.a064273b9aab30886ade848fedf998e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8540c424adc5e592c9e308bec80c93",
+    "url": "/dashboard-app/index.html"
+  },
+  {
+    "revision": "5cdbfe889202f96190ae",
+    "url": "/dashboard-app/static/css/main.f5aed5e8.chunk.css"
+  },
+  {
+    "revision": "654af5ab7a2a2e96c458",
+    "url": "/dashboard-app/static/js/2.22498b88.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/dashboard-app/static/js/2.22498b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdbfe889202f96190ae",
+    "url": "/dashboard-app/static/js/main.07c79b41.chunk.js"
+  },
+  {
+    "revision": "e4a4c8a75ef1f079a714",
+    "url": "/dashboard-app/static/js/runtime-main.5309f3cc.js"
+  }
+]);
